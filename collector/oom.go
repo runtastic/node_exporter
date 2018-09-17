0524e25c6812045e6ffc166141fb163f3c77fe27
Peter Löffler <peter.loeffler@runtastic.com>
2018-09-17 06:36:50 +0200

    add node_oom_pid metric

--- collector/oom.go
+++ collector/oom.go
@@ -7,10 +7,19 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+var pidLabelNames = []string{"time", "program"}
 
 type oomCollector struct {
 	metric []typedDesc
+}
+
+type oomDetails struct {
+	Pid                 float64
+	Timestamp, Program string
 }
 
 func init() {
@@ -22,29 +31,33 @@ func NewOomCollector() (Collector, error) {
 
 	return &oomCollector{
 		metric: []typedDesc{
-			{prometheus.NewDesc(prometheus.BuildFQName(namespace, subsystem, "count"), "counter for out of memory occurrences.", nil, nil), prometheus.CounterValue},
+			{prometheus.NewDesc(prometheus.BuildFQName(namespace, subsystem, "count"), "counter for out of memory killer occurrences.", nil, nil), prometheus.GaugeValue},
+			{prometheus.NewDesc(prometheus.BuildFQName(namespace, subsystem, "pid"), "pid that was killed by the out of memory killer.", pidLabelNames, nil), prometheus.GaugeValue},
 		},
 	}, nil
 }
 
 func (c *oomCollector) Update(ch chan<- prometheus.Metric) error {
-	oom_count, err := grepFile(getSyslogFile(), []byte("Killed process"))
+	oom_count, details, err := grepFile(getSyslogFile(), []byte("Killed process"))
 
 	if err != nil {
 		return fmt.Errorf("couldn't get oom value: %s", err)
 	}
 
 	ch <- c.metric[0].mustNewConstMetric(oom_count)
+	for _, detail := range details {
+		ch <- c.metric[1].mustNewConstMetric(detail.Pid, detail.Timestamp, detail.Program)
+	}
 
 	return err
 }
 
-func grepFile(file string, pat []byte) (oom_count float64, err error) {
+func grepFile(file string, pat []byte) (oom_count float64, details []oomDetails, err error) {
 	patCount := float64(0)
 	f, err := os.Open(file)
 	if err != nil {
 		log.Error(err)
-		return patCount, err
+		return patCount, nil, err
 		defer f.Close()
 	}
 	defer f.Close()
@@ -52,12 +65,19 @@ func grepFile(file string, pat []byte) (oom_count float64, err error) {
 	for scanner.Scan() {
 		if bytes.Contains(scanner.Bytes(), pat) {
 			patCount++
+			logLine := scanner.Text()
+			runes := []rune(logLine)
+			logFields := strings.Fields(logLine)
+			timestamp := string(runes[0:15])
+			pid, _ := strconv.ParseFloat(logFields[8], 64)
+			program := strings.Replace(strings.Replace(logFields[9], "(", "", -1), ")", "", -1)
+			details = append(details, oomDetails{Pid: pid, Timestamp: timestamp, Program: program})
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	return patCount, nil
+	return patCount, details, nil
 }
 
 func getSyslogFile() (logfile string) {
